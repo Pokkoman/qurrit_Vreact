a03a7ef152132c91f180245a4d7266fc27a8c9d5
Surit <surit404@gmail.com>
2021-12-31 00:57:35 +0530

    data to be sent

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,12 @@
 
+from django.http import response
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, UserManager
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import *
-from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
@@ -22,52 +23,66 @@ def register(requests):
     last_name = requests.data['last_name']
 
     if requests.data['user_type'] == 'Customer':
-        user = User.objects.create(
-        username=username,
-        password=password,
-        email=email,
-        first_name=first_name,
-        last_name=last_name,
-    )
-        customer = Customer.objects.create(user = user,programs_bought=[0])
+        user = User.objects.create_user(
+            username=username,
+            password=password,
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
+        )
+        customer = Customer.objects.create(user=user, programs_bought=[0])
         customer.save()
     elif requests.data['user_type'] == 'Trainer':
         user = Trainer.objects.create(
-        username=username,
-        password=password,
-        email=email,
-        first_name=first_name,
-        last_name=last_name,)
+            username=username,
+            password=password,
+            email=email,
+            first_name=first_name,
+            last_name=last_name,)
 
-        trainer = Trainer.objects.create(user = user,programs_created=[0])
+        trainer = Trainer.objects.create(user=user, programs_created=[0])
         trainer.save()
 
     return HttpResponse('hello')
+
 
 @api_view(['POST'])
 def userlogin(requests):
     username = requests.data['username']
     password = requests.data['password']
 
-    user = authenticate(requests,username=username,password=password)
+    user = authenticate(requests, username=username, password=password)
 
     if user is not None:
-    
-        login(requests._request,user)
+
+        login(requests._request, user)
+        usercheck = User.objects.get(username=username)
+
+        try:
+            userdata_customer = Customer.objects.get(user=usercheck)
+        except Customer.DoesNotExist:
+            userdata_customer = None
+
+        try:
+            userdata_trainer = Trainer.objects.get(user=usercheck)
+        except Trainer.DoesNotExist:
+            userdata_trainer = None
+
+        print(userdata_customer.user.first_name)
+        print(userdata_trainer)
+
         print("user logged in")
+        return HttpResponse("hello")
     else:
-        print("error")
+        return Response('error')
 
-
-    return HttpResponse("hello")
 
 @api_view(["POST"])
 def userlogout(requests):
 
     logout(requests._request)
-    
-    return HttpResponse('Logout')
 
+    return HttpResponse('Logout')
 
 
 @api_view(['GET'])
