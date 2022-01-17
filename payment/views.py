8c0e21f9965333c514e19a5195e03ad264cc71d6
Surit <surit404@gmail.com>
2022-01-17 21:37:10 +0530

    payment changes

--- payment/views.py
+++ payment/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 import stripe
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from accounts.models import Customer, Trainer
+from api.models import Workout, Program
 
 # Create your views here.
 
@@ -12,15 +14,27 @@ stripe.api_key = 'sk_test_tR3PYbcVNZZ796tH88S4VQ2u'
 @api_view(['POST'])
 def create_payment(requests):
     try:
-        data = requests.data
+        # data = requests.data
+
+        data = {
+            'programId': 2,
+            'userId': 2
+        }
+
+        program = Program.objects.get(id=data['programId'])
+        cost = program.cost
 
         intent = stripe.PaymentIntent.create(
-            amount=100,
+            amount=cost,
             currency='inr',
             automatic_payment_methods={
                 'enabled': True
             },
         )
+
+        customer = Customer.objects.get(id=data['userId'])
+        customer.programs_bought.append(data['programId'])
+        customer.save()
 
         return Response({
             'clientSecret': intent['client_secret']
