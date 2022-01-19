30da9d43067ffa04436a41d0c821e4272d35c460
Surit <surit404@gmail.com>
2022-01-19 19:46:18 +0530

    overlap error

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('isexist', views.user_email_check, name='check_list'),
     path('login', views.userlogin, name='login'),
     path('logout', views.userlogout, name='logout'),
-    # path('profile', views.user_profile, name='user_profile')
+    path('profile', views.user_profile, name='user_profile')
 
 
 ]
